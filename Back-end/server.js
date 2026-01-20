8d41192fa17cf6b6dc12034cfd2b06670bfdd8c4
bngcminh <buingocminh2k4@gmail.com>
2026-01-20 10:19:20 +0700

    update

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,14 +1,26 @@
 require('dotenv').config();
 
+// Cac thu vien can thiet
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// HTTP logger
+app.use(morgan('combined'));
+
+// Cau hinh cac thu muc tinh
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'../Front-end/Views'));
+
+// app.set(express.static(path.join(__dirname,'../Front-end/Public')));
+app.use(express.static(path.join(__dirname,'../Front-end/Public')));
+
 app.get('/', (req, res) => {
-    res.send('hello word');
+    return res.render('home');
 })
 
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}/`);
-});
+app.listen(port, (req, res) => {
+    console.log(`Server running at http://localhost:${port}/`)
+})
